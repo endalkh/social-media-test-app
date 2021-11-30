1fa936b7ea3fcbd77cdc681d4725a8aed717288f
endalkh <endalkhbel@gmail.com>
2021-11-30 22:30:03 +0300

    add unit test

--- authentication/tests.py
+++ authentication/tests.py
@@ -1,39 +1,54 @@
 from django.test import TestCase
-
-# Create your tests here.
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from rest_framework.test import APITestCase
-from django.contrib.auth.models import User
+from authentication import models as authentication
 from rest_framework import status
 
 
 class AccountsTest(APITestCase):
     def setUp(self):
         # We want to go ahead and originally create a user.
-        self.test_user = User.objects.create_user(
-            "testuser", "test@example.com", "testpassword"
+        self.test_user = authentication.User.objects.create_user(
+            name="admin",
+            email="admin@gmail.com",
+            password="admin",
         )
 
         # URL for creating an account.
-        self.create_url = reverse("account-create")
+        self.signin_url = reverse("authentication:signin")
+        self.signup_url = reverse("authentication:signup")
 
     def test_create_user(self):
         """
         Ensure we can create a new user and a valid token is created with it.
         """
         data = {
-            "username": "foobar",
-            "email": "foobar@example.com",
-            "password": "somepassword",
+            "name": "endalk",
+            "email": "endalk@endalk.com",
+            "password": "endalk",
         }
 
-        response = self.client.post(self.create_url, data, format="json")
+        response = self.client.post(self.signup_url, data, format="json")
 
         # We want to make sure we have two users in the database..
-        self.assertEqual(User.objects.count(), 2)
+        self.assertEqual(authentication.User.objects.count(), 2)
         # And that we're returning a 201 created code.
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         # Additionally, we want to return the username and email upon successful creation.
-        self.assertEqual(response.data["username"], data["username"])
+        self.assertEqual(response.data["name"], data["name"])
+        self.assertEqual(response.data["email"], data["email"])
+        self.assertFalse("password" in response.data)
+
+    def test_login(self):
+        """
+        Ensure we can create a new user and a valid token is created with it.
+        """
+        data = {
+            "email": "admin@gmail.com",
+            "password": "admin",
+        }
+
+        response = self.client.post(self.signin_url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["email"], data["email"])
         self.assertFalse("password" in response.data)
